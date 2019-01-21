9d64d55aa9953e5fd1076e139247518826f419e0
unknown <mritzing@gmail.com>
2019-01-20 19:58:05 -0500

    Added in users

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import {ApolloServer} from "apollo-server-express";
+import "reflect-metadata";
+import * as Express from "express";
+import {buildSchema} from 'type-graphql';
+import {createConnection} from "typeorm";
+
+import {RegisterResolver} from "./modules/user/Register"
+
+
+//bootstrap typegraphql 
+//https://19majkel94.github.io/type-graphql/docs/bootstrap.html
+const main = async() => {
+
+    await createConnection();
+
+    const schema = await buildSchema({
+        resolvers: [RegisterResolver],
+      });
+    
+    const apolloServer = new ApolloServer({schema});
+
+    const app = Express()
+
+    apolloServer.applyMiddleware({app});
+    app.listen(4000, () => {
+        console.log("Server started on localhost 4000/graphql")
+    })
+}
+
+main();
